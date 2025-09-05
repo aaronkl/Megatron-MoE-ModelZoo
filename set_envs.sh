303da5461624b77ba6f438c1ddc102fbcc28a24f
Aaron Klein <kleiaaro@gmail.com>
2025-09-05 10:32:53 +0200

    script to load envs

---
+++ set_envs.sh
@@ -0,0 +1,6 @@
+export MEGATRON_PATH="/leonardo/home/userexternal/aklein00/Megatron-LM"
+export MCORE_RELEASE_VERSION="0.13"
+export CONTAINER_IMAGE="/leonardo_work/AIFAC_L01_028/aklein/container_pretraining_megatron.sif"
+export CLUSTER="leonardo"
+export MEGATRON_CACHE_BASE=$SCRATCH
+export MEGATRON_CACHE_FOLDER="${MEGATRON_CACHE_BASE}/MEGATRON_CACHE/"
